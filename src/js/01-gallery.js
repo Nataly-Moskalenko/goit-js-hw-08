592c0a0dc119c37fe5197a4bcabbd2156c9b605f
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-01-29 13:29:59 +0200

    adds solving the task-01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImageCardsMarkup(images) {
+  return images.map(({ preview, original, description }) => {
+    return `
+ <li>
+ <a class="gallery__item" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+ </a>
+</li>
+`;
+  }).join('');
+};
+
+const gallery = new SimpleLightbox('.gallery a', { captionDelay: 250, });
