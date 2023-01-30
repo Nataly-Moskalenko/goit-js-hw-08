45e2520ae3e768abf12abca4a84c262f388de455
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-01-30 23:32:45 +0200

    Adds @vimeo/player to task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+ player.on('timeupdate', function() {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+   }).catch(function() {    
+});
+  });
+
+  player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function() {    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':           
+            break;
+        default:           
+            break;
+    }
+});
