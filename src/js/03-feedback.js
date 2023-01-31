e6ed1ba866617a96a7193ff5783a4ddc5072e2c2
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-02-01 00:08:32 +0200

    Adds solving the task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+function saveValues(event) {
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify({
+      email: email.value,
+      message: message.value,
+    })
+  );
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({
+    email: email.value,
+    message: message.value,
+  });
+  form.reset();
+  localStorage.clear();
+}
+
+form.addEventListener('input', throttle(saveValues, 500));
+form.addEventListener('submit', handleSubmit);
+
+const parsedValues = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+input.value = parsedValues.email;
+textarea.value = parsedValues.message;
