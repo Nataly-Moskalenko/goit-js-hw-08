c0fb0593ec1d4763eb5b1393d2d2af7b53544243
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-02-21 16:50:19 +0200

    Fixes gallery's card markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,13 +7,11 @@ import '../css/common.css';
 function createImageCardsMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
-      return `
-<li>
- <a class="gallery__item" href="${original}">
-   <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
- </a>
-</li>
-`;
+      return `        
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+        </a>        
+        `;
     })
     .join('');
 }
